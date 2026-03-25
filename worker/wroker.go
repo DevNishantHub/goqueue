af05ac3da47efd0cfbdbeb9cbbb7eb2922ab9aec
Nishant Kumar <nishantkumar211106@gmail.com>
2026-03-25 22:30:39 +0530

    Completed worker.go

--- worker/wroker.go
+++ worker/wroker.go
@@ -1,6 +1,11 @@
 package worker
 
-import "github.com/DevNishantHub/goqueue/broker"
+import (
+	"fmt"
+
+	"github.com/DevNishantHub/goqueue/broker"
+	"github.com/DevNishantHub/goqueue/task"
+)
 
 type Worker struct{
 	broker 		*broker.Broker
@@ -16,5 +21,34 @@ func new (b *broker.Broker) *Worker {
 
 func (w *Worker) Register(name string,fn func(args []interface{})(interface{},error)) {
 	w.registry[name] = fn
+}
+
+func (w *Worker)Run() {
+	for{
+		t,err := w.broker.Dequeue()
+		if err != nil {
+			fmt.Println("Dequeue Error: %w",err)
+			continue
+		}
+		go func(t *task.Task) {
+			fn,ok := w.registry[t.FuncName]
+			if !ok {
+				t.Status = task.StatusError
+				t.Error = fmt.Sprintf("no handler for: %s", t.FuncName)
+				w.broker.SetResult(t.Id, t)
+				return
+			}
+
+			result,err := fn(t.Args)
+			if err != nil {
+				t.Status =task.StatusError
+				t.Error = err.Error()
+			} else {
+				t.Status = task.StatusSuccess
+				t.Error = result.(string)
+			}
+			w.broker.SetResult(t.Id,t)
+		}(t)
+	}
 }
 
